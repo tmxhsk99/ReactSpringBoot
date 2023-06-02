cfd29615252d3f725819e63dea6fed2481f09506
tmxhsk99 <tmxhsk99@naver.com>
2023-06-02 10:57:12 +0900

    feat: 이미지 리소스 추가

---
+++ front/src/component/ImageShowcase.js
@@ -0,0 +1,35 @@
+import "./ImageShowcase.css";
+
+const ImageShowcase = ({title, subTitle, cardImgList}) => {
+    return (
+        <div className="depth image-showcase">
+            <div className="inner">
+                <div className="txt_wrap txt_center">
+                    <h2 className="tit_small">
+                        {title}
+                    </h2>
+                    <p className="sub_txt">
+                        {subTitle}
+                    </p>
+                </div>
+                <ul className="image-showcase_list txt_center">
+                    {
+                        cardImgList.map((cardImg) => {
+                            return (
+                                <li>
+                                    <div className="img_wrap">
+                                        <img alt={cardImg.name} src={cardImg.img}/>
+                                    </div>
+                                    <p className="cont_txt">{cardImg.description}</p>
+                                </li>
+                            )
+                        })
+                    }
+
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default ImageShowcase;
