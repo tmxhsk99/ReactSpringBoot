fd41dd501fa41cb84d7d9c66ac2638bf93e9a047
tmxhsk99 <tmxhsk99@naver.com>
2023-06-05 09:26:04 +0900

    chore : 파일명 수정 ArticleList -> Article

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Route, Routes} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home"
-import ArticleList from "./pages/article/ArticleList"
+import Article from "./pages/article/Article"
 import ArticleAdd from "./pages/article/ArticleAdd"
 import ArticleEdit from "./pages/article/ArticleEdit"
 import ArticleDetail from "./pages/article/ArticleDetail"
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="/article/list" element={<ArticleList/>}></Route>
+        <Route path="/article/list" element={<Article/>}></Route>
         <Route path="/article/edit/:id" element={<ArticleEdit/>}></Route>
         <Route path="/article/add" element={<ArticleAdd/>}></Route>
         <Route path="/article/detail/:id" element={<ArticleDetail/>}></Route>
