6361a68604e3936b150eb157efafe1b70bcd085d
tmxhsk99 <tmxhsk99@naver.com>
2023-06-06 11:07:36 +0900

    feat : API 통신 로직 작성 진행중

--- front/src/pages/article/Article.js
+++ front/src/pages/article/Article.js
@@ -1,15 +1,32 @@
 import Header from "../../component/common/Header";
 import Footer from "../../component/common/Footer";
-import {SITE_NAME,DEFAULT_MENU} from "../../util";
+import {SITE_NAME, DEFAULT_MENU} from "../../util";
+import ArticleHeader from "../../component/article/ArticleHeader";
+import {useEffect} from "react";
+const fetchArticle = async (page,size,title,content) => {
+    const API_DOMAIN = "http://localhost:8080";
+    const response = await fetch(`${API_DOMAIN}/posts?page=${page}&size=${size}&title=${title}&content=${content}`);
+    const responseJson = await response.json();
+    return responseJson;
+};
 
 const Article = () => {
-
+    useEffect(()=>{
+        const posts = fetchArticle();
+        console.log(posts);
+    },[])
     return (
         <>
             <Header
                 title={SITE_NAME}
                 menus={DEFAULT_MENU}/>
-            <div>Article List 페이지</div>
+            <div className="inner">
+                <div className="nes-container is-dark with-title">
+                    <ArticleHeader
+                        title={"전체 글"}
+                    />
+                </div>
+            </div>
             <Footer
                 title={SITE_NAME}
                 menus={DEFAULT_MENU}
