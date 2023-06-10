6d1400cce7866b1ef3b09ff12049b3dd25218a1b
tmxhsk99 <tmxhsk99@naver.com>
2023-06-10 12:26:34 +0900

    chore : 초기화 sql 설정 삭제

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO post (title,content,created_time,updated_time) VALUES ('제목1','내용1',Now(),Now())
-INSERT INTO post (title,content,created_time,updated_time) VALUES ('제목2','내용2',Now(),Now())
-INSERT INTO post (title,content,created_time,updated_time) VALUES ('제목3','내용3',Now(),Now())
