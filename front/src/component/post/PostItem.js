eee6100415d1182e54f5d2474ec83e2d7bdf403f
tmxhsk99 <tmxhsk99@naver.com>
2023-06-13 13:44:17 +0900

    feat : PostItem 컴포넌트 추가

---
+++ front/src/component/post/PostItem.js
@@ -0,0 +1,33 @@
+import "./PostItem.css";
+import {Link} from "react-router-dom";
+
+const PostItem = ({id,title, content, type = "none", createdTime, updatedTime, commentCount = 0, nickName = "익명",viewCount = 0}) => {
+    return (
+        <Link className="PostItem" to={`/post/detail/${id}`}>
+            <div className="image">
+                <i className="fa-regular fa-file-lines"/>
+            </div>
+            <div className="info">
+                <div className="titleContainer">
+                    <span className="title">
+                        <span className="text">
+                            {type != "none" ? <span className="pre">{`[${type}]`}</span> : ""}
+                            {title}
+                        </span>
+                        {commentCount != 0 ? <span className="commentCount">{commentCount}</span> : ""}
+                    </span>
+                </div>
+                <div className="etc">
+                    <div className="nickName">{nickName}</div>
+                    <div className="dot"></div>
+                    <div className="dateTime">{createdTime}</div>
+                    <div className="dot"></div>
+                    <div className="viewCount">{viewCount}</div>
+                </div>
+            </div>
+
+        </Link>
+    );
+}
+
+export default PostItem;
