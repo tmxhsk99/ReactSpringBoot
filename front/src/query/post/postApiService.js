ea71575504036ed0703f0a2a976b8845abdb3656
tmxhsk99 <tmxhsk99@naver.com>
2023-06-16 10:52:27 +0900

    feat : reactQuery 사용 POST 리스트 조회 API 추가

---
+++ front/src/query/post/postApiService.js
@@ -0,0 +1,13 @@
+import {fetcher} from "../queryClient";
+
+/**
+ * 게시글 리스트 요청 유틸함수
+ * @param pageParam
+ * @param sizeParam
+ * @returns {Promise<any>}
+ */
+export const postGetFetcher = ({ page = 1, size = 10 }) => fetcher({
+    method: "GET",
+    path: "/posts",
+    params: { page: page, size: size }
+})
---
+++ front/src/query/queryClient.js
@@ -0,0 +1,68 @@
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+    QueryClient,
+    QueryClientProvider,
+} from 'react-query'
+import {POST_API_DOMAIN} from "../util/util";
+
+export const getQueryClient = (() => {
+    let client = null;
+    return () => {
+        if (!client) client = new QueryClient(
+            {
+                defaultOptions: {
+                    queries: {
+                        cacheTime: 1000 * 60 * 60 * 6,
+                        staleTime: 1000 * 60 * 60,
+                        refetchOnMount:false,
+                        refetchOnReconnect: false,
+                        refetchOnWindowFocus: false,
+                    }
+                }
+            }
+        );
+        return client;
+    }
+})();
+
+export const fetcher = async ({method, path, body, params}) => {
+    //posts?page=${page}&size=${size}
+    let url = `${POST_API_DOMAIN}${path}`;
+    let RequestInit;
+    const fetchOptions = RequestInit = {
+        method,
+        header: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': POST_API_DOMAIN,
+        }
+    }
+
+    if (params) {
+        const searchParams = new URLSearchParams(params);
+        url += '?' + searchParams.toString();
+    }
+
+    if (body) {
+        fetchOptions.body = JSON.stringify(body);
+    }
+
+    const resPostData = await fetch(url, fetchOptions)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`errorCode : ${res.status} \nerrorMsg : ${res.json()}`);
+            }
+            return res.json();
+        })
+        .catch((e) => {
+            console.error(e);
+            return null;
+        });
+    console.log(resPostData);
+    return resPostData;
+}
+
+export const QueryKeys = {
+    POSTS: 'POSTS',
+}
