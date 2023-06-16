f080aa0dcbd5a954b700f200459cd82946eaa1b6
tmxhsk99 <tmxhsk99@naver.com>
2023-06-16 11:01:44 +0900

    refactor : 불필요한 import 삭제

--- front/src/pages/post/PostListPage.js
+++ front/src/pages/post/PostListPage.js
@@ -2,26 +2,21 @@ import Header from "../../component/common/Header";
 import Footer from "../../component/common/Footer";
 import {SITE_NAME, DEFAULT_MENU} from "../../util/util";
 import PostList from "../../component/post/PostList";
-import useGetPost from "../../component/hooks/post/useGetPost";
-import Loading from "../../component/common/Loading";
-import {PostStateContext} from "./Post";
-import {useContext} from "react";
 
 const PostListPage = () => {
-        return (
-            <>
-                <Header
-                    title={SITE_NAME}
-                    menus={DEFAULT_MENU}/>
-                <PostList/>
-                <Footer
-                    title={SITE_NAME}
-                    menus={DEFAULT_MENU}
-                />
-            </>
+    return (
+        <>
+            <Header
+                title={SITE_NAME}
+                menus={DEFAULT_MENU}/>
+            <PostList/>
+            <Footer
+                title={SITE_NAME}
+                menus={DEFAULT_MENU}
+            />
+        </>
 
-        )
-
+    )
 }
 
 export default PostListPage;
