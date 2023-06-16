ca5ac7220fe207a4c0852c830621764c23f55ca4
tmxhsk99 <tmxhsk99@naver.com>
2023-06-16 11:07:17 +0900

    feat : recoil, react-query 관련 태그 추가

--- front/src/App.js
+++ front/src/App.js
@@ -3,15 +3,27 @@ import {Route, Routes} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import Post from "./pages/post/Post";
+import {getQueryClient} from "./query/queryClient";
+import {QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+import {RecoilRoot} from "recoil";
+
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/post/*" element={<Post/>}></Route>
-      </Routes>
-    </div>
-  );
+    const queryClient = getQueryClient();
+
+    return (
+        <RecoilRoot>
+            <QueryClientProvider client={queryClient}>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Home/>}></Route>
+                        <Route path="/post/*" element={<Post/>}></Route>
+                    </Routes>
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                </div>
+            </QueryClientProvider>
+        </RecoilRoot>
+    );
 }
 
 export default App;
