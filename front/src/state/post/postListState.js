344ea5a6335a2b32f85c087ce0705e5a448d1c80
tmxhsk99 <tmxhsk99@naver.com>
2023-06-20 20:07:26 +0900

    refactor : 페이지상태를 게시글상태와 통합
    - 통합에 따른 기존 페이지 정보삭제와
    - 기존 상태 명 변경에 따른 삭제(postList-> posts)

--- front/src/state/post/postListState.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
-
-export const postListState = atom({
-  key: 'postListState',
-  default: {
-    posts: [],
-  },
-});
