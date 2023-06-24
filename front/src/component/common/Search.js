b3109dae1a23edaf9e8e08ef06e8e8d2633c915f
tmxhsk99 <tmxhsk99@naver.com>
2023-06-24 13:20:26 +0900

    fix : 상세 보기에서 목록으로 이동한 경우 이전 검색 조건이 리셋되는 문제 수정

--- front/src/component/common/Search.js
+++ front/src/component/common/Search.js
@@ -1,6 +1,9 @@
 import "./Search.css";
 import BasicNesBtn from "./button/BasicNesBtn";
 import {useState} from "react";
+import {useRecoilValue} from "recoil";
+import {postsState} from "../../state/post/postsState";
+import {postSearchConditionGetKeyword} from "../../util/validationUtil";
 
 /**
  * 검색 타입 + 검색창 + 검색 버튼
@@ -10,26 +13,54 @@ import {useState} from "react";
  * @returns {JSX.Element}
  * @constructor
  */
-const Search = ({searchType, searchEvent, navigate}) => {
-    const [keyword, setKeyword] = useState("");
+const Search = ({searchType, searchEvent}) => {
+    const posts = useRecoilValue(postsState);
+    const searchCondition = posts.searchCondition;
+    const [keyword, setKeyword] = useState(postSearchConditionGetKeyword(searchCondition));
     const [searchTypeState, setSearchTypeState] = useState([searchType[0].value]);
+
+    const selectedSearchType = () => {
+         let result = "";
+         if(searchCondition.title){
+             result = "title";
+         }
+         if(searchCondition.content){
+             result += ",content";
+         }
+         if(searchCondition.nickName){
+             result = "nickName";
+         }
+         return result;
+     }
+
+    const [defaultSearchType, setDefaultSearchType] = useState(selectedSearchType());
+
 
     const handleChangeSelect = (e) => {
         // 검색조건 배열
-        setSearchTypeState(e.target.value.split(","));
         console.log(e.target.value);
+        setSearchTypeState(e.target.value.split(","));
+        setDefaultSearchType(e.target.value);
+
     }
 
     const onChangeKeyword = (e) => {
         setKeyword(e.target.value);
-        console.log(e.target.value);
     }
+
+    // 엔터시 검색 처리
+    const handleKeyDown = (e) => {
+      if (e.key === 'Enter') {
+            searchEvent({type: searchTypeState, keyword: keyword});
+      }
+    };
+
 
     return (
         <div className="Search">
             <div className="search_wrap">
                 <div className="type nes-select">
-                    <select onChange={handleChangeSelect} required id="default_select">
+                    <select value={defaultSearchType} onChange={handleChangeSelect} required id="default_select">
                         {
                             searchType.map((it, idx) => {
                                 return <option key={String(idx)} value={it.value}>{it.name}</option>
@@ -38,7 +69,7 @@ const Search = ({searchType, searchEvent, navigate}) => {
                     </select>
                 </div>
                 <div className="input_wrap nes-field">
-                    <input type="text" maxLength="50" onChange={onChangeKeyword} className="nes-input"/>
+                    <input type="text" maxLength="50" onKeyDown={handleKeyDown} onChange={onChangeKeyword} value={keyword} className="nes-input"/>
                 </div>
                 <BasicNesBtn title={"검색"} onClick={() => {
                     searchEvent({type: searchTypeState, keyword: keyword});
