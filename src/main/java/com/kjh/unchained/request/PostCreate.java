3e35f76c8cf8c797931dd3ebafeb34daf4cf9b19
tmxhsk99 <tmxhsk99@naver.com>
2023-06-27 14:54:17 +0900

    fix : Max는 숫자형만 체크하므로 Size로 변경

--- src/main/java/com/kjh/unchained/request/PostCreate.java
+++ src/main/java/com/kjh/unchained/request/PostCreate.java
@@ -3,8 +3,8 @@ package com.kjh.unchained.request;
 import com.kjh.unchained.exception.InvalidRequest;
 import lombok.*;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -12,12 +12,12 @@ import javax.validation.constraints.NotBlank;
 public class PostCreate {
 
     @NotBlank(message = "타이틀을 입력해주세요.")
-    @Max(value = 50, message = "타이틀은 50자 이내로 입력해주세요.")
+    @Size(max = 50, message = "타이틀은 50자 이내로 입력해주세요.")
     private String title;
 
     // 이걸 달아주면 검증을해준다 . @RequestBody 옆에 @Valid 선언을 해두면
     @NotBlank(message = "내용을 입력해주세요.")
-    @Max(value = 10000, message = "내용은 10000자 이내로 입력해주세요.")
+    @Size(max = 10000, message = "내용은 10000자 이내로 입력해주세요.")
     private String content;
 
     @Builder
