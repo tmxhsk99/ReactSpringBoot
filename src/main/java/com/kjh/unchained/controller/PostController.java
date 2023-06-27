10d9200ba26d6ed33ac1368911921f4eaa702609
tmxhsk99 <tmxhsk99@naver.com>
2023-06-27 16:46:40 +0900

    fix : url 명 수정 및 사용하지 않는 라우트 삭제 - 일반 페이지와 구별을위해 접두 /api 추가 - 사용하지 않는 라우트및 테스트 삭제

--- src/main/java/com/kjh/unchained/controller/PostController.java
+++ src/main/java/com/kjh/unchained/controller/PostController.java
@@ -24,12 +24,12 @@ public class PostController {
     private final PostService postService;
 
 
-    @DeleteMapping("/posts/{postId}")
+    @DeleteMapping("/api/posts/{postId}")
     public void delete(@PathVariable long postId){
         postService.delete(postId);
     }
 
-    @PatchMapping("/posts/{postId}")
+    @PatchMapping("/api/posts/{postId}")
     public PostResponse edit(@PathVariable long postId, @RequestBody @Valid PostEdit request){
         return postService.edit(postId, request);
     }
@@ -39,7 +39,7 @@ public class PostController {
      * @param postSearch
      * @return
      */
-    @GetMapping("/posts")
+    @GetMapping("/api/posts")
     public PostListResponse getList(@ModelAttribute @Valid PostSearch postSearch) {
         log.info("[PostController:/post]"+ postSearch.toString()+"\n");
         PostListResponse result = postService.getList(postSearch);
@@ -51,7 +51,7 @@ public class PostController {
      * /posts -> 글 전체 조회 (검색 + 페이징)
      * /post/(postId) -> 글 단건 조회
      */
-    @GetMapping("/posts/{postId}")
+    @GetMapping("/api/posts/{postId}")
     public PostResponse get(@PathVariable(name = "postId") Long id) {
         PostResponse postResponse = postService.get(id);
         return postResponse;
@@ -64,91 +64,10 @@ public class PostController {
      * @return
      * @throws Exception
      */
-    @PostMapping("/posts/save")
+    @PostMapping("/api/posts")
     public void posts_save(@RequestBody @Valid PostCreate request) throws Exception {
-        // Case1. 저장한 데이터 Entity ->response로 응답하기
-        // Case2. 저장한 데이터 의 primary_id -> response로 응답하기
-        //      Client 에서는 수신한 id를 글 조회 API를 통해서 데이터르 수신 받음
-        // Case3. 응답 필요 없음 -> 클라이언트에서 모든 POST(글) 데이터 context를 잘 관리 함
-        // Bad Case : 서버에서 -> 반드시 이렇게 할껍니다! fix
-        //                  -> 서버에서 차라리 유연한게 대응하는 것이 좋다 -> 코드를 잘 짜야한다!
-        //                  -> 한 번에 일괄적으로 잘 처리 되는 케이스가 없다. -> 잘 관리하는 형태가 중요하다.
         request.validate();
         postService.write(request);
     }
-
-
-    // Http Method
-    // GET , POST , PATCH , DELETE , OPTIONS, HEAD ,PUT , TRACE, CONNECT
-    // 특징을 알아야한다
-    // 글 등록
-    // POST Method
-
-    @PostMapping("/posts_form_urlencoded")
-    public String post_urlencoded(@RequestParam String title, @RequestParam String content) {
-        log.info("title={}, content={}", title, content);
-        return "Hello world";
-    }
-
-
-    /**
-     * 위와 동일하지만 매개변수를 Map 으로 받는 경우
-     */
-    @PostMapping("/posts_pramMap")
-    public String posts_pramMap(@RequestParam Map<String, String> params) {
-        log.info("params={}", params);
-        return "Hello world";
-    }
-
-    /**
-     * 위와 동일하지만 해당 값에대한 클래스를 만들어 받는 경우
-     */
-    @PostMapping("/posts_recv_object")
-    public String posts_recv_object(PostCreate postCreateDto) {
-        log.info("params={}", postCreateDto.toString());
-        return "Hello world";
-    }
-// 데이터를 검증하는 이유
-
-    // 1. client 개발자가 깜빡할 수 있다. 실수로 값을 안보낼수 있다.
-    // 2. client bug로 값이 누락 될 수 있다.
-    // 3. 외부에 나쁜사람이 값을 임의로 조작해서 보낼 수 있다.
-    // 4. DB에 값을 저장할 때 의도치 않은 오류 가 발생 할 수 있다.
-    // 5. 서버 개발자의 편안 함을 위해서
-    @PostMapping("/posts_json")
-    public Map<String, String> posts_json(@RequestBody @Valid PostCreate params, BindingResult result) throws Exception {
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            Map<String, String> errorMap = new HashMap<>();
-            for (FieldError fieldError : fieldErrors) {
-                String fieldName = fieldError.getField(); //에러발생 필드의 들어온값
-                String errorMessage = fieldError.getDefaultMessage();//...에러메시지
-                errorMap.put(fieldName, errorMessage); //에러를 Map에 넣어 둔다.
-            }
-            return errorMap;
-        }
-
-        return new HashMap<>();
-    }
-
-//        String title = params.getTitle();
-//        if(title == null || title.equals("")){
-//            // 이런 종류의 검증이 안좋은 이유
-//            // 1. 빡세다 (노가다)
-//            // 2. 개발팁 -> 무언가 3번 이상 반복작업을 할 때 내가 뭔가 잘못하고 있는 건 아닌지 의심한다.
-//            // 3. 누락가능성
-//            // 4. 생각보다 검증해야할 게 많다. (꼼꼼하지 않을 수 도 있다)
-//            // 이런 경우를 고려 해야 한다.
-//            // {"title" : "           "}
-//            // {"title" : "..수십억 글자"}
-//            throw new Exception("타이틀 값이 없다.");
-//        }
-
-    @PostMapping("/posts_use_controller_advice")
-    public Map<String, String> posts_use_controller_advice(@RequestBody @Valid PostCreate params) throws Exception {
-
-        return new HashMap<>();
-    }
-
 
 }
--- src/test/java/com/kjh/unchained/controller/PostControllerTest.java
+++ src/test/java/com/kjh/unchained/controller/PostControllerTest.java
@@ -56,7 +56,7 @@ class PostControllerTest {
         String json = objectMapper.writeValueAsString(request);
 
         //when
-        mockMvc.perform(post("/posts/save")
+        mockMvc.perform(post("/api/posts")
                         .contentType(APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isBadRequest())
@@ -78,7 +78,7 @@ class PostControllerTest {
         /**
          * expected
          */
-        mockMvc.perform(delete("/posts/{postId}",post.getId()) // PATCH /post/{postId}
+        mockMvc.perform(delete("/api/posts/{postId}",post.getId()) // PATCH /post/{postId}
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -104,7 +104,7 @@ class PostControllerTest {
         /**
          * expected
          */
-        mockMvc.perform(patch("/posts/{postId}",post.getId()) // PATCH /post/{postId}
+        mockMvc.perform(patch("/api/posts/{postId}",post.getId()) // PATCH /post/{postId}
                         .contentType(APPLICATION_JSON).content(objectMapper.writeValueAsString(postEdit)))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -131,7 +131,7 @@ class PostControllerTest {
          * expected
          */
         //mockMvc.perform(get("/posts?page=1&sort=id,desc&size=5") //pagable 생성시 , 파라미터로 size를 넘길수도 있지만 application.yml에 기본설정을 지성할 수 도 있다.
-        mockMvc.perform(get("/posts?page=0&size=10")
+        mockMvc.perform(get("/api/posts?page=0&size=10")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()", is(10)))
@@ -154,7 +154,7 @@ class PostControllerTest {
         postRepository.save(post);
 
         //expected [when + then]
-        mockMvc.perform(get("/posts/{postId}",post.getId())
+        mockMvc.perform(get("/api/posts/{postId}",post.getId())
                 .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(post.getId()))
@@ -178,7 +178,7 @@ class PostControllerTest {
         String jsonString = objectMapper.writeValueAsString(request);
 
         // when
-        mockMvc.perform(post("/posts/save")
+        mockMvc.perform(post("/api/posts")
                         .contentType(APPLICATION_JSON)
                         .content(jsonString)
                 )
@@ -207,7 +207,7 @@ class PostControllerTest {
         String jsonString = objectMapper.writeValueAsString(request);
 
         //제목을 제거한다.
-        mockMvc.perform(post("/posts_use_controller_advice")
+        mockMvc.perform(post("/posts")
                         .contentType(APPLICATION_JSON)
                         .content(jsonString)
                 )
@@ -217,114 +217,5 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
-
-    @Test
-    @DisplayName("/posts 요청시 title 값은 필수다.")
-    void sendJson_required_title() throws Exception {
-        PostCreate request = PostCreate
-                .builder()
-                .content("내용입니다.")
-                .build();
-
-        String jsonString = objectMapper.writeValueAsString(request);
-        //제목을 제거한다.
-        mockMvc.perform(post("/posts_json")
-                        .contentType(APPLICATION_JSON)
-                        .content(jsonString)
-                )
-                .andExpect(status().isOk())
-                //.andExpect(content().string("Hello world")) // 결과 내용 검증
-                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요.")) //타이틀 에러시 joson 에러 메시지 검증
-                .andDo(print());
-    }
-
-
-    // 글 제목
-    // 글 내용
-    // 사용자
-    // id
-    // name
-    // level
-
-    // 기존 form 방식의 key value 방식으 애매하다.
-    // 그러므로 json 으로 보내야한다.
-    @Test
-    @DisplayName("/posts_json 로 post 요청 contentType = APPLICATION_JSON , content에 실제 내용 설정")
-    void sendJson() throws Exception {
-        PostCreate request = PostCreate
-                .builder()
-                .title("제목입니다.")
-                .content("내용입니다.")
-                .build();
-
-        String jsonString = objectMapper.writeValueAsString(request);
-        //expected
-        mockMvc.perform(post("/posts_json")
-                        .contentType(APPLICATION_JSON)
-                        .content(jsonString)
-                )
-                .andExpect(status().isOk())
-                //.andExpect(content().string("Hello world")) //return Type  변경으로 주석처리
-                .andDo(print());
-    }
-
-
-//================================================================================
-    // 글 제목
-    // 글 내용
-    // 예전에는 application/x-www-form-urlencoded 를 많이 사용함
-
-    /**
-     * Form Encoded 형태로 전달한다.
-     */
-    @Test
-    @DisplayName("/posts_recv_object 로 post 요청 contentType = APPLICATION_FORM_URLENCODED")
-    void simpleControllerRecvObjTest() throws Exception {
-        //expected
-        mockMvc.perform(post("/posts_recv_object")
-                        .contentType(APPLICATION_FORM_URLENCODED)
-                        .param("title", "글 제목 입니다.")
-                        .param("content", "글 내용입니다")
-                )
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello world"))
-                .andDo(print());
-    }
-
-    /**
-     * Form Encoded 형태로 전달한다.
-     */
-    @Test
-    @DisplayName("/posts_pramMp 로 post 요청 contentType = APPLICATION_FORM_URLENCODED")
-    void simplePostControllerParamMapTest() throws Exception {
-        //expected
-        mockMvc.perform(post("/posts_pramMap")
-                        .contentType(APPLICATION_FORM_URLENCODED)
-                        .param("title", "글 제목 입니다.")
-                        .param("content", "글 내용입니다")
-                )
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello world"))
-                .andDo(print());
-    }
-
-
-    /**
-     * Form Encoded 형태로 전달한다.
-     */
-    @Test
-    @DisplayName("/posts_form_urlencoded 로 post 요청 contentType = APPLICATION_FORM_URLENCODED")
-    void simplePostFormUrlEncodedTest() throws Exception {
-        //expected
-        mockMvc.perform(post("/posts_form_urlencoded")
-                        .contentType(APPLICATION_FORM_URLENCODED)
-                        .param("title", "글 제목 입니다.")
-                        .param("content", "글 내용입니다")
-                )
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello world"))
-                .andDo(print());
-    }
-
 
 }
