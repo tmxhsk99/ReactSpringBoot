11f1b163d5a503df509d3509eb61bff9f2639781
tmxhsk99 <tmxhsk99@naver.com>
2023-06-29 10:56:45 +0900

    feat : 타입으로 글쓰기,글수정 페이지 구분

--- front/src/component/post/Article.js
+++ front/src/component/post/Article.js
@@ -1,12 +1,13 @@
 import "./Article.css";
 import SearchAndWrite from "../common/SearchAndWrite";
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import BasicNesBtn from "../common/button/BasicNesBtn";
 import {postCreateValidation} from "../../util/validationUtil";
-import {postCreateFetcher} from "../../query/post/postApiService";
+import {postCreateFetcher, postUpdateFetcher} from "../../query/post/postApiService";
 import {useNavigate} from "react-router-dom";
+import Loading from "../common/Loading";
 
-const Article = ({post}) => {
+const Article = ({post, type}) => {
     const [textArea, setTextArea] = useState("");
     const textareaRef = useRef(null);
     const titleTextRef = useRef(null);
@@ -42,47 +43,69 @@ const Article = ({post}) => {
         if (!postCreateValidation({title, content})) {
             return;
         }
-        await void postCreateFetcher({title, content});
+        if (type === "edit") {
+            await void postUpdateFetcher({title, content, id: post.id});
+        }
+        if (type === "add") {
+            await void postCreateFetcher({title, content});
+        }
         navigate("/post/list");
+
     }
 
-    return (
-        <div className="Article">
-            <div className="title nes-field">
-                <label htmlFor="name_field">제목</label>
-                <input type="text"
-                       maxLength={100}
-                       className="nes-input"
-                       ref={titleTextRef}
-                       value={titleText}
-                       onChange={onChangeTitleText}
-                />
-            </div>
-            <div className="content">
-                <label htmlFor="textarea_field">내용</label>
-                <textarea className="nes-textarea"
-                          ref={textareaRef}
-                          value={textArea}
-                          onChange={(event) => {
-                              handleTextareaChange(event);
-                              onChangeTextArea(event);
-                          }}
-                          maxLength={10000}
-                />
-                <SearchAndWrite
-                    rightChildren={`내용 : ${textArea.length}/10000`}
-                />
-            </div>
-            <SearchAndWrite
-                centerChildren={
-                    <BasicNesBtn title={"등록"}
-                                 onClick={() => onSubmitPost({title: titleText, content: textArea})}
-                    />
-                }
-            />
-        </div>
+    useEffect(() => {
+            if (post) {
+                setTitleText(post.title);
+                setTextArea(post.content);
+            }
+        }
+        , [post]);
 
-    );
+    if (type === "edit" && !titleText && !textArea) {
+        return (
+            <div className="Article">
+                <Loading></Loading>
+            </div>
+        )
+    } else {
+        return (
+            <div className="Article">
+                <div className="title nes-field">
+                    <label htmlFor="name_field">제목</label>
+                    <input type="text"
+                           maxLength={100}
+                           className="nes-input"
+                           ref={titleTextRef}
+                           value={titleText}
+                           onChange={onChangeTitleText}
+                    />
+                </div>
+                <div className="content">
+                    <label htmlFor="textarea_field">내용</label>
+                    <textarea className="nes-textarea"
+                              ref={textareaRef}
+                              value={textArea}
+                              onChange={(event) => {
+                                  handleTextareaChange(event);
+                                  onChangeTextArea(event);
+                              }}
+                              maxLength={10000}
+                    />
+                    <SearchAndWrite
+                        rightChildren={`내용 : ${textArea.length}/10000`}
+                    />
+                </div>
+                <SearchAndWrite
+                    centerChildren={
+                        <BasicNesBtn title={"등록"}
+                                     onClick={() => onSubmitPost({title: titleText, content: textArea})}
+                        />
+                    }
+                />
+            </div>
+
+        );
+    }
 }
 
 export default Article;
