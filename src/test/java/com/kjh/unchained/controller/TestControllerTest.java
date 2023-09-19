039713f992fd4451956a97cc3af1b2e3f8cf6aa1
juhyeon <tmxhsk99@naver.com>
2023-09-20 08:23:36 +0900

    test : 인증테스트 쿠키를 사용하는 방식으로 수정
    요청 헤더에 AccessToken을 넣어서 검증하는 방식에서
    Cookie를 통해 인증토큰을 확인하므로 쿠키를 넣어 요청하는 방식으로
    테스트수정

--- src/test/java/com/kjh/unchained/controller/TestControllerTest.java
+++ src/test/java/com/kjh/unchained/controller/TestControllerTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import javax.servlet.http.Cookie;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -51,8 +53,10 @@ class TestControllerTest {
             @DisplayName("세션 정보를 반환한다.")
             @Test
             void It_returns_session_information() throws Exception {
+                Cookie sessionCookie = new Cookie("SESSION", session.getAccessToken());
+
                 mockMvc.perform(get("/api/test/session")
-                                .header("Authorization", session.getAccessToken())
+                                .cookie(sessionCookie)
                                 .contentType("application/json"))
                         .andExpect(status().isOk())
                         .andDo(print());
@@ -67,8 +71,9 @@ class TestControllerTest {
             @DisplayName("UnAuthorized 예외를 반환한다.")
             @Test
             void It_returns_session_information() throws Exception {
+                Cookie sessionCookie = new Cookie("SESSION", session.getAccessToken() +"INVALID");
                 mockMvc.perform(get("/api/test/session")
-                                .header("Authorization", session.getAccessToken() +"INVALID")
+                                .cookie(sessionCookie)
                                 .contentType("application/json"))
                         .andExpect(status().isUnauthorized())
                         .andDo(print());
