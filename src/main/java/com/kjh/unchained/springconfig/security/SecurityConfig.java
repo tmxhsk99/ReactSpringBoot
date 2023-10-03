4b9a79eecff4216b7a124c75542cd70d2e10b0cb
tmxhsk99 <tmxhsk99@naver.com>
2023-10-03 15:45:35 +0900

    feat : 커스텀 시큐리티 핸들러 추가

--- src/main/java/com/kjh/unchained/springconfig/security/SecurityConfig.java
+++ src/main/java/com/kjh/unchained/springconfig/security/SecurityConfig.java
@@ -3,12 +3,16 @@ package com.kjh.unchained.springconfig.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kjh.unchained.domain.User;
 import com.kjh.unchained.repository.jpa.user.UserRepository;
+import com.kjh.unchained.springconfig.security.filter.EmailPasswordAuthFilter;
 import com.kjh.unchained.springconfig.security.handler.Http401Handler;
 import com.kjh.unchained.springconfig.security.handler.Http403Handler;
 import com.kjh.unchained.springconfig.security.handler.LoginFailHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -18,8 +22,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
@@ -31,6 +38,9 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     private final ObjectMapper objectMapper;
+    private final UserRepository userRepository;
+
+    private String LOGIN_REQUEST_URL = "/api/auth/login";
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -48,24 +58,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
 
-        String loginRequestURL = "/api/auth/login";
 
         http
-                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(new AntPathRequestMatcher(loginRequestURL)).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher(LOGIN_REQUEST_URL)).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/api/auth/signup")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/user")).hasAnyRole("USER")
                         .requestMatchers(new AntPathRequestMatcher("/admin")).hasAnyRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .formLogin(form ->
-                        form.loginPage(loginRequestURL)
-                                .loginProcessingUrl(loginRequestURL)
-                                .usernameParameter("email")
-                                .passwordParameter("password")
-                                .defaultSuccessUrl("/")
-                                .failureHandler(new LoginFailHandler(objectMapper))
+                .addFilterBefore(
+                        usernamePasswordAuthFilter(),
+                        UsernamePasswordAuthenticationFilter.class
                 )
                 .rememberMe(rememberMe ->
                         rememberMe.rememberMeParameter("remember") // 어떤 파라미터로 값이 넘어와야 자동로그인으로 설정할건지 설정
@@ -80,11 +84,40 @@ public class SecurityConfig {
                             e.authenticationEntryPoint(new Http401Handler(objectMapper)); // 인증이 안됐을때
                         }
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(withDefaults());
 
 
         return http.build();
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService(userRepository));
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public EmailPasswordAuthFilter usernamePasswordAuthFilter() throws Exception {
+        EmailPasswordAuthFilter filter = new EmailPasswordAuthFilter(LOGIN_REQUEST_URL, objectMapper);
+        filter.setAuthenticationManager(authenticationManager());
+        // 기본적인 기능들은 있는것 가져다 써야한다.
+        filter.setAuthenticationSuccessHandler(new SimpleUrlAuthenticationSuccessHandler("/"));
+        filter.setAuthenticationFailureHandler(new LoginFailHandler(objectMapper));
+
+        // 셋팅을 안해주면 세션이 발급이 안된다.
+        filter.setSecurityContextRepository(new HttpSessionSecurityContextRepository());
+
+        // remember-me 설정
+        SpringSessionRememberMeServices rememberMeServices = new SpringSessionRememberMeServices();
+        rememberMeServices.setAlwaysRemember(true); // 유효기간만큼의 세션을 발급한다.
+        rememberMeServices.setValiditySeconds(3600 * 24 * 30);
+        filter.setRememberMeServices(rememberMeServices);
+        return filter;
     }
 
     @Bean
---
+++ src/main/java/com/kjh/unchained/springconfig/security/filter/EmailPasswordAuthFilter.java
@@ -0,0 +1,48 @@
+package com.kjh.unchained.springconfig.security.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class EmailPasswordAuthFilter extends AbstractAuthenticationProcessingFilter {
+
+    private final ObjectMapper objectMapper;
+
+    public EmailPasswordAuthFilter(String loginUrl,ObjectMapper objectMapper) {
+        // 로그인 시도하는 주소를 설정한다.
+        super(loginUrl);
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        EmailPassword emailPassword = objectMapper.readValue(request.getInputStream(), EmailPassword.class);
+        // 토큰을 만든다 , 커스터마이징을 하지는 않는다.
+
+        // 토큰을 만들어서 AuthenticationManager에게 넘겨준다.
+        UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(
+                emailPassword.getEmail(),
+                emailPassword.getPassword()
+        );
+        token.setDetails(this.authenticationDetailsSource.buildDetails(request));
+        return this.getAuthenticationManager().authenticate(token);
+
+    }
+
+    @Getter
+    private static class EmailPassword {
+        private String email;
+        private String password;
+    }
+}
