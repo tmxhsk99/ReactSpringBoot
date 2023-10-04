5885686dbbdc881c6e4f818f8c664f5b30ca1580
juhyeon <tmxhsk99@naver.com>
2023-10-05 08:33:44 +0900

    test : REST DOC 관련 테스트 WithMockUser 추가

--- src/test/java/com/kjh/unchained/controller/PostControllerDocTest.java
+++ src/test/java/com/kjh/unchained/controller/PostControllerDocTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -71,6 +72,9 @@ public class PostControllerDocTest {
 
     @Test
     @DisplayName("글 등록 테스트")
+    @WithMockUser(username = "admin@unchained.com",
+            roles = {"ADMIN"}
+    )
     void RestDocsTest_post_save() throws Exception {
         //given
         PostCreate request = PostCreate.builder()
